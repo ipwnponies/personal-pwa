997ea67a73a9ba1f75e30bf9ae92e2b3ce390bfb
John Nguyen <ipwnponies@gmail.com>
2022-08-28 00:00:00 -0700

    [random-app] Bare UI structure

--- pages/random/index.jsx
+++ pages/random/index.jsx
@@ -1,5 +1,43 @@
 import React, { useState } from 'react';
 
-export default function () {
-  return <p>Placeholder for app</p>;
+export default function Random() {
+  return (
+    <div style={{ display: 'grid', maxWidth: '50rem', margin: 'auto' }}>
+      <h1>RNG</h1>
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+        <div>
+          Lower
+          <input
+            id="lowerBound"
+            type="number"
+            max="100"
+            min="1"
+            placeholder="1"
+          />
+        </div>
+        <div>
+          Upper
+          <input
+            id="upperBound"
+            type="number"
+            max="100"
+            min="1"
+            placeholder="6"
+          />
+        </div>
+      </div>
+      <div style={{ display: 'grid', gridTemplateColumns: '1fr 1fr' }}>
+        <p>
+          How many dice?
+          <input id="numDice" type="number" max="20" min="1" placeholder="1" />
+        </p>
+      </div>
+      <input
+        type="button"
+        value="Roll"
+        style={{ width: '100%', maxWidth: '20rem', margin: 'auto' }}
+      />
+      <div>Results placeholder, including sum</div>
+    </div>
+  );
 }
