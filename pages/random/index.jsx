2c1870b24d53c19bc64027d5e360beb5e8ff4751
John Nguyen <ipwnponies@gmail.com>
2022-09-05 21:58:50 -0700

    [random-app] Moves inline styles to component css

--- pages/random/index.jsx
+++ pages/random/index.jsx
@@ -1,68 +1,40 @@
 import React, { useState } from 'react';
 
+import styles from './index.module.css';
+
 export default function Random() {
   return (
-    <div
-      style={{
-        display: 'grid',
-        margin: 'auto',
-        height: '100vh',
-        aspectRatio: '9/16',
-      }}
-    >
-      <div
-        style={{
-          display: 'grid',
-          gridTemplateColumns: 'repeat(4, 1fr)',
-          gap: '1em',
-          gridAutoRows: 'fit-content(10%) repeat(2, 1px) 300px',
-          gridTemplateAreas: `
-            'h h h h'
-            'lowerPrompt lowerInput upperPrompt upperInput'
-            'dicePrompt dicePrompt . diceInput'
-            'roll roll roll roll'
-            'result result result result'
-          `,
-        }}
-      >
-        <h1 style={{ gridArea: 'h' }}>RNG</h1>
-        <span style={{ gridArea: 'lowerPrompt' }}>Lower</span>
-        <input
-          id="lowerBound"
-          type="number"
-          max="100"
-          min="1"
-          placeholder="1"
-          style={{ gridArea: 'lowerInput', maxHeight: '3rem' }}
-        />
-        <span style={{ gridArea: 'upperPrompt' }}>Upper</span>
-        <input
-          id="upperBound"
-          type="number"
-          max="100"
-          min="1"
-          placeholder="6"
-          style={{ gridArea: 'upperInput', maxHeight: '3rem' }}
-        />
-        <span style={{ gridArea: 'dicePrompt' }}>How many dice?</span>
-        <input
-          id="numDice"
-          type="number"
-          max="20"
-          min="1"
-          placeholder="1"
-          style={{ gridArea: 'diceInput', maxHeight: '3rem' }}
-        />
-        <input
-          type="button"
-          value="Roll"
-          style={{ width: '100%', maxWidth: '20rem', margin: 'auto' }}
-          style={{ gridArea: 'roll' }}
-        />
-        <div style={{ gridArea: 'result' }}>
-          Results placeholder, including sum
-        </div>
-      </div>
+    <div className={styles.grid}>
+      <h1 className={styles.h}>RNG</h1>
+      <span className={styles.lowerPrompt}>Lower</span>
+      <input
+        id="lowerBound"
+        type="number"
+        max="100"
+        min="1"
+        placeholder="1"
+        className={styles.lowerInput}
+      />
+      <span className={styles.upperPrompt}>Upper</span>
+      <input
+        id="upperBound"
+        type="number"
+        max="100"
+        min="1"
+        placeholder="6"
+        className={styles.upperInput}
+      />
+      <span className={styles.dicePrompt}>How many dice?</span>
+      <input
+        id="numDice"
+        type="number"
+        max="20"
+        min="1"
+        placeholder="1"
+        className={styles.diceInput}
+      />
+      <input type="button" value="Roll" className={styles.roll} />
+      <div className={styles.result}>Results placeholder, including sum</div>
     </div>
   );
 }
