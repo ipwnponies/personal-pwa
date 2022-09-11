b290df06af4b28044153f80553b02e7facb52038
John Nguyen <ipwnponies@gmail.com>
2022-09-11 12:23:48 -0700

    Add states for input

--- pages/random/index.jsx
+++ pages/random/index.jsx
@@ -3,6 +3,10 @@ import React, { useState } from 'react';
 import styles from './index.module.css';
 
 export default function Random() {
+  const [lowerBound, setLowerBound] = useState(1);
+  const [upperBound, setUpperBound] = useState(6);
+  const [numDice, setNumDice] = useState(1);
+
   return (
     <div className={styles.grid}>
       <h1 className={styles.h}>RNG</h1>
@@ -12,8 +16,9 @@ export default function Random() {
         type="number"
         max="100"
         min="1"
-        placeholder="1"
         className={styles.lowerInput}
+        value={lowerBound}
+        onChange={(e) => setLowerBound(parseInt(e.target.value, 10))}
       />
       <span className={styles.upperPrompt}>Upper</span>
       <input
@@ -21,8 +26,9 @@ export default function Random() {
         type="number"
         max="100"
         min="1"
-        placeholder="6"
         className={styles.upperInput}
+        value={upperBound}
+        onChange={(e) => setUpperBound(parseInt(e.target.value, 10))}
       />
       <span className={styles.dicePrompt}>How many dice?</span>
       <input
@@ -30,11 +36,18 @@ export default function Random() {
         type="number"
         max="20"
         min="1"
-        placeholder="1"
         className={styles.diceInput}
+        value={numDice}
+        onChange={(e) => setNumDice(parseInt(e.target.value, 10))}
       />
       <input type="button" value="Roll" className={styles.roll} />
-      <div className={styles.result}>Results placeholder, including sum</div>
+      <div className={styles.result}>
+        {`Lower bound is ${lowerBound}`}
+        <br />
+        {`upper bound is ${upperBound}`}
+        <br />
+        {`number of dice is ${numDice}`}
+      </div>
     </div>
   );
 }
