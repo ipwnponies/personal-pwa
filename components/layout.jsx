b0f77f5ad4d49a15f359286470f8f4b4b70dd63f
John Nguyen <ipwnponies@gmail.com>
2025-11-22 08:10:09 -0800

    Add favicons

--- components/layout.jsx
+++ components/layout.jsx
@@ -25,41 +25,87 @@ function metadata() {
       <meta name="msapplication-tap-highlight" content="no" />
       <meta name="theme-color" content="#000000" />
 
-      <link rel="apple-touch-icon" href="/icons/touch-icon-iphone.png" />
-      <link rel="apple-touch-icon" sizes="152x152" href="/icons/touch-icon-ipad.png" />
-      <link rel="apple-touch-icon" sizes="180x180" href="/icons/touch-icon-iphone-retina.png" />
-      <link rel="apple-touch-icon" sizes="167x167" href="/icons/touch-icon-ipad-retina.png" />
+      <link
+        rel="apple-touch-icon"
+        sizes="96x96"
+        href="/icons/android-launchericon-96-96.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="144x144"
+        href="/icons/android-launchericon-144-144.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="192x192"
+        href="/icons/android-launchericon-192-192.png"
+      />
+      <link
+        rel="apple-touch-icon"
+        sizes="512x512"
+        href="/icons/android-launchericon-512-512.png"
+      />
 
-      <link rel="icon" type="image/png" sizes="32x32" href="/icons/favicon-32x32.png" />
-      <link rel="icon" type="image/png" sizes="16x16" href="/icons/favicon-16x16.png" />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="48x48"
+        href="/icons/android-launchericon-48-48.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="72x72"
+        href="/icons/android-launchericon-72-72.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="96x96"
+        href="/icons/android-launchericon-96-96.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="144x144"
+        href="/icons/android-launchericon-144-144.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="192x192"
+        href="/icons/android-launchericon-192-192.png"
+      />
+      <link
+        rel="icon"
+        type="image/png"
+        sizes="512x512"
+        href="/icons/android-launchericon-512-512.png"
+      />
       <link rel="manifest" href="/manifest.json" />
-      <link rel="mask-icon" href="/icons/safari-pinned-tab.svg" color="#5bbad5" />
-      <link rel="shortcut icon" href="/favicon.ico" />
-      <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500" />
+      <link
+        rel="stylesheet"
+        href="https://fonts.googleapis.com/css?family=Roboto:300,400,500"
+      />
 
       <meta name="twitter:card" content="summary" />
       <meta name="twitter:url" content="https://yourdomain.com" />
       <meta name="twitter:title" content="PWA App" />
       <meta name="twitter:description" content="Best PWA App in the world" />
-      <meta name="twitter:image" content="https://yourdomain.com/icons/android-chrome-192x192.png" />
+      <meta
+        name="twitter:image"
+        content="https://yourdomain.com/icons/android-chrome-192x192.png"
+      />
       <meta name="twitter:creator" content="@DavidWShadow" />
       <meta property="og:type" content="website" />
       <meta property="og:title" content="PWA App" />
       <meta property="og:description" content="Best PWA App in the world" />
       <meta property="og:site_name" content="PWA App" />
       <meta property="og:url" content="https://yourdomain.com" />
-      <meta property="og:image" content="https://yourdomain.com/icons/apple-touch-icon.png" />
-
-      {/* <!-- apple splash screen images --> */}
-      {/* <!-- */}
-      <link href="/images/apple_splash_2048.png" sizes="2048x2732" />
-      <link href="/images/apple_splash_1668.png" sizes="1668x2224" />
-      <link href="/images/apple_splash_1536.png" sizes="1536x2048" />
-      <link href="/images/apple_splash_1125.png" sizes="1125x2436" />
-      <link href="/images/apple_splash_1242.png" sizes="1242x2208" />
-      <link href="/images/apple_splash_750.png" sizes="750x1334" />
-      <link href="/images/apple_splash_640.png" sizes="640x1136" />
-      {/* --> */}
+      <meta
+        property="og:image"
+        content="https://yourdomain.com/icons/apple-touch-icon.png"
+      />
     </>
   );
 }
@@ -68,12 +114,23 @@ export default function Layout({ children, home }) {
   return (
     <div className={styles.container}>
       <Head>
-        <link rel="icon" href="/favicon.ico" />
-        <meta name="description" content="Learn how to build a personal website using Next.js" />
-        <meta name="og:image" content={`https://og-image.vercel.app/${encodeURI(siteTitle)}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`} />
+        <link rel="icon" href="/icons/favicon.ico" />
+        <meta
+          name="description"
+          content="Learn how to build a personal website using Next.js"
+        />
+        <meta
+          name="og:image"
+          content={`https://og-image.vercel.app/${encodeURI(
+            siteTitle,
+          )}.png?theme=light&md=0&fontSize=75px&images=https%3A%2F%2Fassets.vercel.com%2Fimage%2Fupload%2Ffront%2Fassets%2Fdesign%2Fnextjs-black-logo.svg`}
+        />
         <meta name="og:title" content={siteTitle} />
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="viewport" content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover" />
+        <meta
+          name="viewport"
+          content="minimum-scale=1, initial-scale=1, width=device-width, shrink-to-fit=no, user-scalable=no, viewport-fit=cover"
+        />
         {metadata()}
       </Head>
       <header className={styles.header}>
