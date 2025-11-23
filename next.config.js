52aa55313548e4aa165e124d356ad3d099644f4c
John Nguyen <ipwnponies@gmail.com>
2025-11-23 00:37:02 -0800

    Fix service worker to be cache-first strategy

--- next.config.js
+++ next.config.js
@@ -1,24 +1,35 @@
 const withPWA = require('next-pwa');
 const defaultRuntimeCaching = require('next-pwa/cache');
 
+const runtimeCaching = defaultRuntimeCaching.map((entry) => {
+  if (entry.options?.cacheName !== 'others') return entry;
+  const { networkTimeoutSeconds, ...options } = entry.options || {};
+  return {
+    ...entry,
+    handler: 'CacheFirst',
+    options,
+  };
+});
+
 module.exports = withPWA({
   pwa: {
     dest: 'public',
+    dynamicStartUrl: false,
     // cacheOnFrontEndNav: true,
-    // runtimeCaching: [
-    //   {
-    //     urlPattern: /\/posts\/ssg-ssr/,
-    //     method: "GET",
-    //     handler: "CacheOnly",
-    //     options: {
-    //       cacheName: "todoApp-api",
-    //       expiration: {
-    //         maxEntries: 64,
-    //         maxAgeSeconds: 24 * 60 * 60, // 24 hours
-    //       },
-    //     },
-    //   },
-    //   ...defaultRuntimeCaching,
-    // ],
+    runtimeCaching: [
+      {
+        urlPattern: /\/random/,
+        method: 'GET',
+        handler: 'CacheOnly',
+        options: {
+          cacheName: 'random',
+          expiration: {
+            maxEntries: 64,
+            maxAgeSeconds: 24 * 60 * 60, // 24 hours
+          },
+        },
+      },
+      ...runtimeCaching,
+    ],
   },
 });
